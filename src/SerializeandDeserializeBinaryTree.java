2353a3dd47b66c06bf0823cf1b849c666338f20d
sbturan <sb_turan@hotmail.com>
2017-07-17 22:27:43 +0300

    today

--- src/SerializeandDeserializeBinaryTree.java
+++ src/SerializeandDeserializeBinaryTree.java
@@ -5,7 +5,8 @@ public class SerializeandDeserializeBinaryTree {
 	public static void main(String[] args) {
 		SerializeandDeserializeBinaryTree s=new SerializeandDeserializeBinaryTree();
 		Codec c=s.new Codec();
-		TreeNode deserialize = c.deserialize("[1,2,null,3,null,4,null,5]");
+		TreeNode deserialize = c.deserialize("[1,2,3,null,null,4,5]");
+		//"[1,2,3,null,null,4,5]"
 		System.out.println(c.serialize(deserialize));
 		
 		
@@ -44,27 +45,34 @@ public class SerializeandDeserializeBinaryTree {
 			String[] numbers = data.substring(1, data.length() - 1).split(",");
 			if (numbers.length == 0||numbers[0].length()==0)
 				return null;
-			TreeNode[] nodes = new TreeNode[numbers.length];
-			nodes[0] = new TreeNode(Integer.valueOf(numbers[0]));
-			for (int i = 1; i < numbers.length; i++) {
-				String cur = numbers[i];
-				if ("null".equals(cur)) {
-					continue;
+			int i=0;
+			TreeNode root=new TreeNode(Integer.valueOf(numbers[i++]));
+			Queue<TreeNode> queue=new LinkedList<>();
+			queue.add(root);
+			while(!queue.isEmpty()){
+				TreeNode cur = queue.poll();
+				TreeNode left=null,right=null;
+				if(i<numbers.length&&!numbers[i].equals("null")){
+					left=new TreeNode(Integer.valueOf(numbers[i]));
 				}
-				TreeNode curNode = new TreeNode(Integer.valueOf(cur));
-				 if(i%2==1){
-					 int j=(i-1)/2;
-					 while(nodes[j]==null) j++;
-					 nodes[j].left=curNode;
-					 
-				 }else{
-					 int j=(i-2)/2;
-					 while(nodes[j]==null) j++;
-					 nodes[j].right=curNode;
-				 }
-				nodes[i] = curNode;
+					i++;
+				
+				if(i<numbers.length&&!numbers[i].equals("null")){
+					right=new TreeNode(Integer.valueOf(numbers[i]));
+				}
+				i++;
+				
+				if(left!=null){
+					cur.left=left;
+				    queue.add(left);			
+				}
+				if(right!=null){
+					cur.right=right;
+				    queue.add(right);			
+				}
 			}
-			return nodes[0];
+			
+			return root;
 		}
 	}
 }
