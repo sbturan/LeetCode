58cf0a57a39338eb538918dbe79298655b01e0ea
SECKINBT <SECKINBT@TURANS.intertech.denizbank.com>
2017-10-23 16:58:14 +0300

    task schedular is not completed

---
+++ src/TaskScheduler.java
@@ -0,0 +1,12 @@
+public class TaskScheduler {
+	public static void main(String[] args) {
+		char[] tasks = new char[] { 'A', 'A', 'A', 'B', 'B', 'B' };
+		int n = 2;
+		System.out.println(new TaskScheduler().leastInterval(tasks, n));
+	}
+
+	public int leastInterval(char[] tasks, int n) {
+		 
+		return 0;
+	}
+}
