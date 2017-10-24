ba88330ff7037a3c60060b051e33f8f30f3e682c
SECKINBT <SECKINBT@TURANS.intertech.denizbank.com>
2017-10-24 09:47:36 +0300

    TaskScheduler

--- src/TaskScheduler.java
+++ src/TaskScheduler.java
@@ -1,12 +1,24 @@
+import java.util.Arrays;
+
 public class TaskScheduler {
 	public static void main(String[] args) {
-		char[] tasks = new char[] { 'A', 'A', 'A', 'B', 'B', 'B' };
-		int n = 2;
+		char[] tasks = new char[] { 'A', 'A', 'A','B', 'B' };
+		int n = 3;
 		System.out.println(new TaskScheduler().leastInterval(tasks, n));
 	}
 
 	public int leastInterval(char[] tasks, int n) {
-		 
-		return 0;
+
+		   int[] map = new int[26];
+	        for (char c: tasks)
+	            map[c - 'A']++;
+	        Arrays.sort(map);
+	        int max_val = map[25] - 1, idle_slots = max_val * n;
+	        for (int i = 24; i >= 0 && map[i] > 0; i--) {
+	            idle_slots -= Math.min(map[i], max_val);
+	        }
+	        return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
 	}
+
+	
 }
