7210f281f41b2ac17726bee54a722a0c2459a203
SECKINBT <SECKINBT@TURANS.intertech.denizbank.com>
2018-03-21 11:06:22 +0300

    rotate digits better solution

--- src/RotatedDigits.java
+++ src/RotatedDigits.java
@@ -5,41 +5,25 @@ public class RotatedDigits {
 		System.out.println(r.rotatedDigits(89));
 	}
 	public int rotatedDigits(int N) {
-	   char[] rotatableDigits=new char[] {'0','1','2','5','6','8','9'};
-	   boolean[] rotatables=new boolean[10];
-	   for(char i:rotatableDigits) {
-		   rotatables[i-'0']=true;
-	   }
-	   char[] changeDigits=new char[] {'2','5','6','9'};
-	   boolean[] changables=new boolean[10];
-	   for(char i:changeDigits) {
-		   changables[i-'0']=true;
-	   }
-      
-       int result=0;
-       for(int i=1;i<=N;i++) {
-    	   String current=String.valueOf(i);
-    	  if(canRotatable(current,rotatables,changables))
-    		   result++;
-       }
-       
-       return result;
+		   int[] dp = new int[N + 1];
+	        int count = 0;
+	        for(int i = 0; i <= N; i++){
+	            if(i < 10){
+	                if(i == 0 || i == 1 || i == 8) dp[i] = 1;
+	                else if(i == 2 || i == 5 || i == 6 || i == 9){
+	                    dp[i] = 2;
+	                    count++;
+	                }
+	            } else {
+	                int a = dp[i / 10], b = dp[i % 10];
+	                if(a == 1 && b == 1) dp[i] = 1;
+	                else if(a >= 1 && b >= 1){
+	                    dp[i] = 2;
+	                    count++;
+	                }
+	            }
+	        }
+	        return count;
 	}
 	
-	private boolean canRotatable(String number, boolean[] rotatables,boolean[] changables) {
-		char[] charArray = number.toCharArray();
-		boolean containsDifference=false;
-		for(int i=0;i<charArray.length;i++) {
-			char c=charArray[i];
-			
-			if(!rotatables[c-'0']) {
-				return false;
-			}
-			if(changables[c-'0']) {
-				containsDifference=true;
-			}
-		}
-		
-		return containsDifference;
-	}
 }
