845ba81006d8bd168f05a871e4b8292107b1c87c
SECKINBT <SECKINBT@TURANS.intertech.denizbank.com>
2018-03-30 14:08:38 +0300

    s

---
+++ src/MinimumNumberofArrowstoBurstBalloons.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class MinimumNumberofArrowstoBurstBalloons {
+	public static void main(String[] args) {
+		MinimumNumberofArrowstoBurstBalloons m=new MinimumNumberofArrowstoBurstBalloons();
+		System.out.println(m.findMinArrowShots(new int[][] {{10,16},{2,8},{1,6},{7,12}}));
+		
+	}
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) {
+            return 0;
+        }
+        Arrays.sort(points, (a, b) -> a[1] - b[1]);
+        int arrowPos = points[0][1];
+        int arrowCnt = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (arrowPos >= points[i][0]) {
+                continue;
+            }
+            arrowCnt++;
+            arrowPos = points[i][1];
+        }
+        return arrowCnt;
+    			
+    }
+
+
+
+}
