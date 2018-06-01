3527e89d6b8a5e2307777f20dc277d28a7b73224
sbturan <sb_turan@hotmail.com>
2018-06-01 17:04:20 +0300

    today

--- src/PermutationSequence.java
+++ src/PermutationSequence.java
@@ -1,47 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
 
 public class PermutationSequence {
 	public static void main(String[] args) {
-		PermutationSequence p=new PermutationSequence();
-		System.out.println(p.getPermutation(4,13));
+
 	}
 
 	public String getPermutation(int n, int k) {
-		boolean[] arr = new boolean[n+1];
-		return helper(arr, n, k, n);
-
-	}
-
-	private String helper(boolean[] arr, int n, int k, int orgN) {
-		if (n <= 2) {
-			int first = 0;
-			int second = 0;
-			for (int i = 1; i < n + 1; i++) {
-				if (arr[i] == false) {
-					if (first == 0) {
-						first = i;
-					} else {
-						second = i;
-						break;
-					}
-				}
-			}
-			if(n==1) return Integer.toString(first);
-			if (k == 0) {
-				return Integer.toString(first).concat(Integer.toString(second));
-			} else {
-				return Integer.toString(second).concat(Integer.toString(first));
-			}
-
+		List<Integer> num = new LinkedList<Integer>();
+		for (int i = 1; i <= n; i++)
+			num.add(i);
+		int[] fact = new int[n]; // factorial
+		fact[0] = 1;
+		for (int i = 1; i < n; i++)
+			fact[i] = i * fact[i - 1];
+		k = k - 1;
+		StringBuilder sb = new StringBuilder();
+		for (int i = n; i > 0; i--) {
+			int ind = k / fact[i - 1];
+			k = k % fact[i - 1];
+			sb.append(num.get(ind));
+			num.remove(ind);
 		}
-		int val = (int)Math.ceil((double) k/ getFac(n - 1));
-		String s = Integer.toString(val);
-		arr[val]=true;
-		int sec = (k +1) % (n - 1);
-		s=s.concat(helper(arr, n-1, sec, orgN));
-		return s;
+		return sb.toString();
 	}
-	private int getFac(int n){
-		if(n==1) return 1;
-		return getFac(n-1)*n;
-	}
+
 }
--- src/WordLadder.java
+++ src/WordLadder.java
@@ -1,24 +1,114 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 public class WordLadder {
 	public static void main(String[] args) {
-		
+
 		/*
-		 * beginWord = "hit"
-endWord = "cog"
-wordList = ["hot","dot","dog","lot","log"]*/
-		WordLadder wr=new WordLadder();
-		List<String> wordList=new ArrayList<>();
-		String arr[]=new String[]{"a","b","c"};
+		 * beginWord = "hit" endWord = "cog" wordList =
+		 * ["hot","dot","dog","lot","log","cog"]
+		 */
+		WordLadder wr = new WordLadder();
+		List<String> wordList = new ArrayList<>();
+		String arr[] = new String[] { "kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip",
+				"kay", "per", "val", "mes", "ohs", "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub",
+				"lob", "rue", "fry", "lit", "rex", "jan", "cot", "bid", "ali", "pay", "col", "gum", "ger", "row", "won",
+				"dan", "rum", "fad", "tut", "sag", "yip", "sui", "ark", "has", "zip", "fez", "own", "ump", "dis", "ads",
+				"max", "jaw", "out", "btu", "ana", "gap", "cry", "led", "abe", "box", "ore", "pig", "fie", "toy", "fat",
+				"cal", "lie", "noh", "sew", "ono", "tam", "flu", "mgm", "ply", "awe", "pry", "tit", "tie", "yet", "too",
+				"tax", "jim", "san", "pan", "map", "ski", "ova", "wed", "non", "wac", "nut", "why", "bye", "lye", "oct",
+				"old", "fin", "feb", "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob", "for", "joe", "ivy", "fan",
+				"age", "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag", "jar", "arm", "lot",
+				"tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit", "doe", "gas", "rte",
+				"ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin", "don", "tug", "fay", "vic", "boo",
+				"nam", "ave", "buy", "sop", "but", "orb", "fen", "paw", "his", "sub", "bob", "yea", "oft", "inn", "rod",
+				"yam", "pew", "web", "hod", "hun", "gyp", "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub",
+				"ere", "dig", "era", "cat", "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye",
+				"ani", "and", "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog",
+				"nun", "par", "wan", "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus", "rev", "axe",
+				"ion", "six", "ila", "lao", "mom", "mas", "pro", "few", "opt", "poe", "art", "ash", "oar", "cap", "lop",
+				"may", "shy", "rid", "bat", "sum", "rim", "fee", "bmw", "sky", "maj", "hue", "thy", "ava", "rap", "den",
+				"fla", "auk", "cox", "ibo", "hey", "saw", "vim", "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog",
+				"ram", "let", "see", "zit", "maw", "nix", "ate", "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo",
+				"any", "dow", "cod", "bed", "vet", "ham", "sis", "hex", "via", "fir", "nod", "mao", "aug", "mum", "hoe",
+				"bah", "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem", "who", "bet", "gos", "son", "ear", "spy",
+				"kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin", "nil", "mia", "ewe", "hit", "fix",
+				"sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad", "rye", "pap", "bog", "gut", "ito",
+				"woe", "our", "ado", "sin", "mad", "ray", "hon", "roy", "dip", "hen", "iva", "lug", "asp", "hui", "yak",
+				"bay", "poi", "yep", "bun", "try", "lad", "elm", "nat", "wyo", "gym", "dug", "toe", "dee", "wig", "sly",
+				"rip", "geo", "cog", "pas", "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio", "yon",
+				"dec", "leg", "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc",
+				"moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt", "lid", "pun",
+				"ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub", "low", "did", "tin", "get",
+				"gte", "sox", "lei", "mig", "fig", "lon", "use", "ban", "flo", "nov", "jut", "bag", "mir", "sty", "lap",
+				"two", "ins", "con", "ant", "net", "tux", "ode", "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow",
+				"sal", "sic", "ted", "wot", "del", "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err",
+				"him", "all", "pad", "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was",
+				"cab", "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot", "ora",
+				"tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not", "act", "gil", "rut",
+				"ala", "ape", "rig", "cid", "god", "duo", "lin", "aid", "gel", "awl", "lag", "elf", "liz", "ref", "aha",
+				"fib", "oho", "tho", "her", "nor", "ace", "adz", "fun", "ned", "coo", "win", "tao", "coy", "van", "man",
+				"pit", "guy", "foe", "hid", "mai", "sup", "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft",
+				"alb", "len", "air", "pug", "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun",
+				"pat", "ten", "mob" };
 		for (String string : arr) {
 			wordList.add(string);
 		}
-		System.out.println(wr.ladderLength("a", "c", wordList));
+		System.out.println(wr.ladderLength("cet", "ism", wordList));
 	}
 
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        
-    	return 0;
-    }
+	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+		Set<String> dict = new HashSet<>(wordList);
+		if(!dict.contains(endWord)) return 0;
+		Queue<String> queue = new LinkedList<String>();
+		queue.add(beginWord);
+		queue.add(null);
+
+		// Mark visited word
+		Set<String> visited = new HashSet<String>();
+		visited.add(beginWord);
+
+		int level = 1;
+
+		while (!queue.isEmpty()) {
+			String str = queue.poll();
+
+			if (str != null) {
+				// Modify str's each character (so word distance is 1)
+				for (int i = 0; i < str.length(); i++) {
+					char[] chars = str.toCharArray();
+
+					for (char c = 'a'; c <= 'z'; c++) {
+						chars[i] = c;
+
+						String word = new String(chars);
+
+						// Found the end word
+						if (word.equals(endWord))
+							return level + 1;
+
+						// Put it to the queue
+						if (dict.contains(word) && !visited.contains(word)) {
+							queue.add(word);
+							visited.add(word);
+						}
+					}
+				}
+			} else {
+				level++;
+
+				if (!queue.isEmpty()) {
+					queue.add(null);
+				}
+			}
+		}
+
+		return 0;
+
+	}
+
 }
