f9ba9e8e89769fbac98c89f3f1ec580203c37665
seçkin baran <sbturan@gmail.com>
2020-01-06 02:04:51 +0300

    today

---
+++ src/AccountsMerge.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AccountsMerge {
+	public static void main(String[] args) {
+		List<List<String>> accounts = new ArrayList<>();
+		String[][] accountsArray = new String[][] { { "David", "David0@m.co", "David1@m.co" },
+				{ "David", "David3@m.co", "David4@m.co" }, { "David", "David4@m.co", "David5@m.co" },
+				{ "David", "David2@m.co", "David3@m.co" }, { "David", "David1@m.co", "David2@m.co" } };
+		for (String[] a : accountsArray) {
+			accounts.add(Arrays.asList(a));
+		}
+		System.out.println(new AccountsMerge().accountsMerge(accounts));
+	}
+
+	class DSU {
+		int[] parent;
+
+		public DSU() {
+			parent = new int[10001];
+			for (int i = 0; i <= 10000; ++i)
+				parent[i] = i;
+		}
+
+		public int find(int x) {
+			if (parent[x] != x)
+				parent[x] = find(parent[x]);
+			return parent[x];
+		}
+
+		public void union(int x, int y) {
+			parent[find(x)] = find(y);
+		}
+	}
+
+		public List<List<String>> accountsMerge(List<List<String>> accounts) {
+			DSU dsu = new DSU();
+			Map<String, String> emailToName = new HashMap();
+			Map<String, Integer> emailToID = new HashMap();
+			int id = 0;
+			for (List<String> account : accounts) {
+				String name = "";
+				for (String email : account) {
+					if (name == "") {
+						name = email;
+						continue;
+					}
+					emailToName.put(email, name);
+					if (!emailToID.containsKey(email)) {
+						emailToID.put(email, id++);
+					}
+					dsu.union(emailToID.get(account.get(1)), emailToID.get(email));
+				}
+			}
+
+			Map<Integer, List<String>> ans = new HashMap();
+			for (String email : emailToName.keySet()) {
+				int index = dsu.find(emailToID.get(email));
+				ans.computeIfAbsent(index, x -> new ArrayList()).add(email);
+			}
+			for (List<String> component : ans.values()) {
+				Collections.sort(component);
+				component.add(0, emailToName.get(component.get(0)));
+			}
+			return new ArrayList(ans.values());
+		}
+}
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,94 @@
-public class Test {
-	public static void main(String[] args) {
-          
-	}
-}
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.LinkedHashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//public class Test {
+//	public static void main(String[] args) {
+//		Test t = new Test();
+////		
+////		System.out.println(
+////				t.minimumHours(4, 5, possibleFeatures, numFeatureRequests, featureRequests));
+//	}
+//	   int minimumHours(int rows, int columns, List<List<Integer> > grid)
+//	    { 
+//		    if(rows==0||columns==0 ||grid==null || grid.get(0)==null) {
+//		    	return 0;
+//		    }
+//	        int result=0;
+//	        int[][] matrix=new int[rows][columns];
+//	     	for(int i=0;i<rows;i++) {
+//        		for(int j=0;j<columns;j++) {
+//        	       matrix[i][j]=grid.get(i).get(j);		
+//        		}
+//	     	}
+//	     	int oneCount=0;
+//	        while (oneCount<rows*columns) {
+//	        	oneCount=0;
+//	        	int[][] matrixNew=new int[rows][columns];
+//	        	for(int i=0;i<rows;i++) {
+//	        		for(int j=0;j<columns;j++) {
+//	        			if(matrix[i][j]==1) {
+//	        				matrixNew[i][j]=1;
+//	        				oneCount++;
+//	        				if(i>0) {
+//	        					matrixNew[i-1][j]=1;
+//	        				}
+//	        				if(i<rows-1) {
+//	        					matrixNew[i+1][j]=1;
+//	        				}
+//	        				if(j>0) {
+//	        					matrixNew[i][j-1]=1;
+//	        				}
+//	        				if(j<columns-1) {
+//	        					matrixNew[i][j+1]=1;
+//	        				}
+//	        			}
+//	        		}
+//	        	}
+//	        	  for (int i = 0; i < matrixNew.length; i++) {
+//	        		  matrix[i] = Arrays.copyOf(matrixNew[i], matrixNew[i].length);
+//	        	  }
+//	        	result++;
+//	        }
+//	        return result-1;
+//	    }
+//	public ArrayList<String> popularNFeatures(int numFeatures, int topFeatures, List<String> possibleFeatures,
+//			int numFeatureRequests, List<String> featureRequests) {
+//		if(topFeatures>numFeatures) {
+//			return new ArrayList<>(featureRequests);
+//		}
+//		if (topFeatures == 0 || featureRequests == null || possibleFeatures == null || numFeatures == 0
+//				|| numFeatureRequests == 0) {
+//			return new ArrayList<String>();
+//		}
+//
+//		HashMap<String, Integer> counts = new HashMap<>();
+//		for (String pf : possibleFeatures) {
+//			for (String fr : featureRequests) {
+//				if (fr.toLowerCase().indexOf(pf.toLowerCase()) > -1) {
+//					counts.put(pf, counts.getOrDefault(pf, 0) + 1);
+//
+//				}
+//			}
+//		}
+////		Map<String, Integer> sortedMap = counts.entrySet().stream()
+////				.sorted((Map.Entry.<String, Integer>comparingByValue().reversed()).)
+////				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+//		ArrayList<String> result = new ArrayList<String>();
+//		Set<String> keySet = sortedMap.keySet();
+//		int i = 0;
+//		for (String s : keySet) {
+//			result.add(s);
+//			i++;
+//			if (i == topFeatures) {
+//				break;
+//			}
+//		}
+//		return result;
+//	}
+//}
