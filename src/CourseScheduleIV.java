8d92fb5e26e99dd8c9723c9af68d8235844f99e1
Seçkin Baran Turan <Sekin Baran Turan@192.168.1.21>
2020-09-22 22:49:16 +0300

    fix

--- src/CourseScheduleIV.java
+++ src/CourseScheduleIV.java
@@ -7,34 +7,46 @@ import java.util.Set;
 public class CourseScheduleIV {
 	public static void main(String[] args) {
 		CourseScheduleIV c = new CourseScheduleIV();
-		System.out.println(c.checkIfPrerequisite(5, new int[][] {{3,4},{2,3},{1,2},{0,1}}, new int[][] {{0,4},{4,0},{1,3},{3,0}}));
+		System.out.println(c.checkIfPrerequisite(5, new int[][] { { 3, 4 }, { 2, 3 }, { 1, 2 }, { 0, 1 } },
+				new int[][] { { 0, 4 }, { 4, 0 }, { 1, 3 }, { 3, 0 } }));
 	}
-	   public List<Boolean> checkIfPrerequisite(int n, int[][] prerequisites, int[][] queries) {
-	    	List<Boolean> result=new ArrayList<Boolean>();
-	    	HashMap<Integer,Set<Integer>> map=new HashMap<Integer,Set<Integer>>();
-	    	for(int[] i:prerequisites) {
-	    		Set<Integer> orDefault = map.getOrDefault(i[1], new HashSet<Integer>());
-	    		orDefault.add(i[0]);
-	    		map.put(i[1],orDefault);
-	    	}
-	    	for(int i[]:queries) {
-	    		result.add(helper(map, i));
-	    	}
-	    	return result;
-	    }
-	    private boolean helper(HashMap<Integer,Set<Integer>> map,int[] elem) {
-	    	if(!map.containsKey(elem[1])) {
-	    		return false;
-	    	}
-	    	Set<Integer> set = map.get(elem[1]);
-			if(set.contains(elem[0])) {
-	    		return true;
-	    	}
-	    	for(int i:set) {
-	    		if(helper(map, new int[] {elem[0],i})) {
-	    			return true;
-	    		}
-	    	}
-	    	return false;
-	    }
+
+	public List<Boolean> checkIfPrerequisite(int n, int[][] prerequisites, int[][] queries) {
+		int[][] dp = new int[n + 1][n + 1];
+		List<Boolean> result = new ArrayList<Boolean>();
+		Set<Integer>[] map=new HashSet[n+1];
+		for (int[] i : prerequisites) {
+			if(map[i[1]]==null) {
+				map[i[1]]=new HashSet<Integer>();
+			}
+			map[i[1]].add(i[0]);
+		}
+		for (int i[] : queries) {
+			result.add(helper(map, i[0], i[1], dp));
+		}
+		return result;
+	}
+
+	private boolean helper(Set<Integer>[] map, int elem0, int elem1, int[][] dp) {
+
+		if (map[elem1]==null) {
+			return false;
+		}
+		if (dp[elem0][elem1] != 0) {
+			return dp[elem0][elem1] == 1;
+		}
+		Set<Integer> set = map[elem1];
+		if (set.contains(elem0)) {
+			dp[elem0][elem1] = 1;
+			return true;
+		}
+		for (int i : set) {
+			if (helper(map, elem0, i, dp)) {
+				dp[elem0][elem1] = 1;
+				return true;
+			}
+		}
+		dp[elem0][elem1] = -1;
+		return false;
+	}
 }
