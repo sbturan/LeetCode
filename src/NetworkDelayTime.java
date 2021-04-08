5b935ffebdcd4f9f357b9bb30c1fc93be55108a9
seckin baran turan <sb_turan@hotmail.com>
2021-04-09 00:23:53 +0300

    today

--- src/NetworkDelayTime.java
+++ src/NetworkDelayTime.java
@@ -1,50 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class NetworkDelayTime {
 	public static void main(String[] args) {
 		System.out.println(
-				new NetworkDelayTime().networkDelayTime(new int[][] { { 1, 2, 1 }, { 2, 3, 2 }, { 1, 3, 2 } }, 3, 1));
+				new NetworkDelayTime().networkDelayTime(new int[][] {{1,2,1},{2,3,7},{1,3,4},{2,1,2}}, 3, 1));
 	}
 
-	public int networkDelayTime(int[][] times, int N, int K) {
-		boolean[] visited = new boolean[N + 1];
-		int[][] time = new int[N + 1][N + 1];
-		HashMap<Integer, List<Integer>> ways = new HashMap<>();
-		for (int i[] : times) {
-			time[i[0]][i[1]] = i[2];
-			List<Integer> orDefault = ways.getOrDefault(i[0], new ArrayList<Integer>());
-			orDefault.add(i[1]);
-			ways.put(i[0], orDefault);
+	public int networkDelayTime(int[][] times, int n, int k) {
+		HashMap<Integer,List<int[]>> map=new HashMap<>();
+		for(int[] i:times){
+			map.putIfAbsent(i[0],new ArrayList<>());
+			map.get(i[0]).add(new int[]{i[1],i[2]});
 		}
-		int result = helper(K, ways, time, visited);
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i]) {
+		int[] receiveTimes=new int[n+1];
+		Arrays.fill(receiveTimes,Integer.MAX_VALUE);
+		receiveTimes[k] = 0;
+		receiveTimes[0]=-1;
+		helper(0,map,receiveTimes,k);
+		int result=0;
+		for(int i:receiveTimes){
+			if(i==Integer.MAX_VALUE)
 				return -1;
-			}
+			result=Math.max(result,i);
 		}
 		return result;
-
 	}
-
-	private int helper(int K, HashMap<Integer, List<Integer>> ways, int[][] time, boolean[] visited) {
-		visited[K] = true;
-		if (!ways.containsKey(K)) {
-			return 0;
+	private void helper(int curTime,HashMap<Integer,List<int[]>> map,int[] receiveTimes,int node){
+		if(!map.containsKey(node))
+			return;
+		for(int[] i:map.get(node)){
+			    if(receiveTimes[i[0]]>curTime+i[1]){
+					receiveTimes[i[0]]=curTime+i[1];
+					helper(curTime+i[1],map,receiveTimes,i[0]);
+				}
 		}
-
-		int result = 0;
-		List<Integer> list = new ArrayList<Integer>();
-		for (int i : ways.get(K)) {
-			if (!visited[i]) {
-				list.add(i);
-			}
-			visited[i] = true;
-		}
-		for (int i : list) {
-			result = Math.max(result, time[K][i] + helper(i, ways, time, visited));
-		}
-		return result;
 	}
 }
