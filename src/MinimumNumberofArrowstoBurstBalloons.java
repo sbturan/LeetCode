e53b0e812030ec03008eb7e3d792f2113365ac28
seckin baran turan <sb_turan@hotmail.com>
2022-06-04 17:03:36 +0300

    fix

--- src/MinimumNumberofArrowstoBurstBalloons.java
+++ src/MinimumNumberofArrowstoBurstBalloons.java
@@ -7,19 +7,24 @@ public class MinimumNumberofArrowstoBurstBalloons {
 		
 	}
     public int findMinArrowShots(int[][] points) {
-            if (points.length == 0)
-                return 0;
-            Arrays.sort(points, (i, j) -> i[1] - j[1]);
-            int result = 1;
-            int[] last = points[0];
-            for (int[] p : points) {
-                if (p[0] > last[1] || p[1] < last[0]) {
-                    result++;
-                }
-                last[0] = Math.max(last[0], p[0]);
-                last[1] = Math.min(last[1], p[1]);
+        if(points.length==0)
+            return 0;
+        Arrays.sort(points,(i,j)->i[1]-j[1]);
+        int result=1;
+        int[] last=points[0];
+        for(int[] p:points){
+            if(p[0]<=last[1]&&p[1]>=last[0]){
+
+                last[0]=Math.max(last[0],p[0]);
+                last[1]=Math.min(last[1],p[1]);
+            }else{
+                result++;
+                last[0]=p[0];
+                last[1]=p[1];
             }
-            return result;
+
+        }
+        return result;
         }
     }
 
