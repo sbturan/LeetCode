1f2249c18d95fe4a6604530c079ff159b590c899
seckin baran turan <sb_turan@hotmail.com>
2023-04-23 19:13:46 +0300

    today

--- src/OddEvenJump.java
+++ src/OddEvenJump.java
@@ -5,43 +5,28 @@ public class OddEvenJump {
 		System.out.println(new OddEvenJump().oddEvenJumps(new int[] { 10, 13, 12, 14, 15 }));
 	}
 
-	public int oddEvenJumps(int[] A) {
-		if (A.length < 2)
-			return A.length;
-		if (A.length == 2) {
-			if (A[0] <= A[1])
-				return 2;
-			return 1;
-		}
-
-		boolean even[] = new boolean[A.length];
-		boolean odd[] = new boolean[A.length];
-		even[even.length - 1] = true;
-		odd[odd.length - 1] = true;
+	public int oddEvenJumps(int[] arr) {
+		boolean[][] dp=new boolean[arr.length][2];
+		dp[dp.length-1][0]=true;
+		dp[dp.length-1][1]=true;
+		int result=1;
 		TreeMap<Integer,Integer> map=new TreeMap<>();
-		map.put(A[A.length-1], A.length-1);
-		for (int i = A.length - 2; i > -1; i--) {
-			if(map.containsKey(A[i])) {
-				even[i]=odd[map.get(A[i])];
-				odd[i]=even[map.get(A[i])];
-			}else {
-				Integer lowerEntry = map.lowerKey(A[i]);
-				Integer higherEntry = map.higherKey(A[i]);
-				if(lowerEntry!=null) {
-					even[i]=odd[map.get(lowerEntry)];
-				}
-				if(higherEntry!=null) {
-					odd[i]=even[map.get(higherEntry)];
-				}
+		map.put(arr[arr.length-1],arr.length-1);
+		for(int i=arr.length-2;i>-1;i--){
+			Integer lower=map.lowerKey(arr[i]+1);
+			Integer higher=map.higherKey(arr[i]-1);
+			if(lower!=null){
+				dp[i][0]=dp[map.get(lower)][1];
 			}
-			map.put(A[i], i);
-		}
-		int result = 0;
-		for (boolean i : odd) {
-			if (i)
+			if(higher!=null){
+				dp[i][1]=dp[map.get(higher)][0];
+			}
+			if(dp[i][1]){
 				result++;
+			}
+			map.put(arr[i],i);
+
 		}
 		return result;
 	}
-
 }
